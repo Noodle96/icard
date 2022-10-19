1804140d420cb5bb0d26f05811cbfefbe9335dd7
Noodle96 <jorgealfredomarino2@gmail.com>
2022-10-19 01:21:57 -0500

    initial proyect

---
+++ icard_react/src/App.js
@@ -0,0 +1,22 @@
+//React Function Component snnipet 'run rfc'
+
+import React from 'react'
+//import {Button} from 'semantic-ui-react'
+import "./App.scss";
+import {Navigation} from "./routes";
+//import {ClientLayout} from "./layouts"
+
+export default function App() {
+  return (
+    <div >
+        <Navigation />
+    </div>
+  );
+  
+}
+//<Button primary>Primary</Button>
+//<Button secondary>Secondary</Button> 
+//<h1 className='App__titulo'>Hola Russell</h1>
+//<div className='App'>
+//<Navigation />         
+//</div>
