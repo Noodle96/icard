32ca263c755366776ad207ecb9f8084ffe806837
Noodle96 <jorgealfredomarino2@gmail.com>
2022-10-27 10:41:13 -0500

    override del modelo Users to encriptar el password

--- icard_django/icard/users/api/views.py
+++ icard_django/icard/users/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from django.contrib.auth.hashers import make_password
 
 from users.models import User
 from users.api.serializer import UserSerializer
@@ -9,3 +10,7 @@ class UserApiViewSet(ModelViewSet):
     permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
+        return super().create(request, *args, **kwargs)
